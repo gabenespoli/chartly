416b833dcedf9d76113a7c66771dd05044dd0768
Gabe Nespoli <gabenespoli@gmail.com>
2025-05-04 09:30:45 -0400

    Add flt.selectbox, many filter features

--- chartly/filter.py
+++ chartly/filter.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import pandas as pd
 import polars as pl
 import streamlit as st
@@ -7,7 +9,7 @@ from chartly import utils
 
 
 class Filter:
-    def __init__(self, id: str, filters: dict):
+    def __init__(self, id: str, filters: dict = {}, label_visibility: str = "visible"):
         """
         Args:
             id: A unique identifier for the filter. This is required for using the
@@ -17,27 +19,60 @@ class Filter:
         """
         self.id = id
         self.filters = filters
+        self.label_visibility = label_visibility  # visible, collapsed, or hidden
+        self.col_names = {}
+        self.filter_types = {}
+        self.bypass_options = {}
+
+    def selectbox(
+        self,
+        label: str,
+        options: list,
+        default=None,
+        col_name: str = None,
+        placeholder: str = None,
+        label_visibility: str = None,
+        filter_type: str = "eq",  # eq, gte, lte, gt, lt
+        bypass_option=None,
+        **kwargs,
+    ):
+        """Add a filter to the self.filters dictionary using a streamlit selectbox
+        widget."""
+        self.col_names[label] = col_name or label
+        default = default or options[0]
+        key = f"{self.id}_{label}_{filter_type}"
+        self.filter_types[label] = filter_type
+        self.bypass_options[label] = bypass_option
+        self.filters[label] = st.selectbox(
+            label=label,
+            options=options,
+            index=options.index(st.session_state.get(key) or default),
+            placeholder=placeholder or label,
+            label_visibility=label_visibility or self.label_visibility,
+            key=key,
+            **kwargs,
+        )
 
     def multiselect(
         self,
-        col_name: str,
+        label: str,
         options: list,
         default: list = [],
-        label: str = None,
+        col_name: str = None,
         placeholder: str = None,
-        label_visibility: str = "collapsed",
+        label_visibility: str = None,
         **kwargs,
     ):
         """Add a filter to the self.filters dictionary using a streamlit multiselect
         widget."""
-        label = None if label == "" else label
-        key = f"{self.id}_{col_name}"
-        self.filters[col_name] = st.multiselect(
-            label=label or col_name,
+        self.col_names[label] = col_name or label
+        key = f"{self.id}_{label}"
+        self.filters[label] = st.multiselect(
+            label=label,
             options=options,
             default=st.session_state.get(key) or default,
-            placeholder=placeholder or label or col_name,
-            label_visibility=label_visibility,
+            placeholder=placeholder or label,
+            label_visibility=label_visibility or self.label_visibility,
             key=key,
             **kwargs,
         )
@@ -118,16 +153,41 @@ def combine_filters(filter1: Filter, filter2: Filter) -> Filter:
 
 @st.cache_data(ttl=None, hash_funcs={Filter: hash_func, pl.DataFrame: utils.pl2pd})
 def filter_data(
-    df: pl.DataFrame, flt: Filter, return_size_too: bool = False
+    df: pl.DataFrame,
+    flt: Filter,
+    names: Union[str, list] = None,
+    return_size_too: bool = False,
 ) -> pl.DataFrame:
-    if isinstance(df, pd.DataFrame):
-        df = pl.DataFrame(df)
+    """
+    names: A subset of filter keys to filter.
+    return_size_too: If True, return a tuple with first the filtered dataframe, and
+        second a dataframe showing the count of rows after each filter step.
+    """
     df_size = {"Total": df.shape[0]}
+    if names is None:
+        names = flt.filters.keys()
+    elif not isinstance(names, list):
+        names = [names]
     for k, v in flt.filters.items():
-        if v is not None and v != []:
-            if k in df.columns:
-                df = df.filter(col(k).is_in(v))
-                df_size[k] = df.shape[0]
+        col_name = flt.col_names.get(k)
+        bypass_option = flt.bypass_options.get(k)
+        filter_type = flt.filter_types.get(k)
+        if col_name is not None and col_name in df.columns and k in names:
+            if isinstance(v, list):
+                if v != []:
+                    df = df.filter(col(col_name).is_in(v))
+            elif v != bypass_option and filter_type is not None:
+                if filter_type == "eq":
+                    df = df.filter(col(col_name) == v)
+                elif filter_type == "gte":
+                    df = df.filter(col(col_name) >= v)
+                elif filter_type == "lte":
+                    df = df.filter(col(col_name) <= v)
+                elif filter_type == "gt":
+                    df = df.filter(col(col_name) > v)
+                elif filter_type == "lt":
+                    df = df.filter(col(col_name) < v)
+            df_size[k] = df.shape[0]
     if return_size_too:
         return df, df_size
     return df
