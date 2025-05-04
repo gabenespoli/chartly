3bbf508d9043e7824fd77082a3137fd4b11c8fd8
Gabe Nespoli <gabenespoli@gmail.com>
2025-05-04 13:50:22 -0400

    Add filter_data to init

--- chartly/__init__.py
+++ chartly/__init__.py
@@ -4,12 +4,14 @@
 
 from chartly.charts import Chart
 from chartly.filter import Filter
+from chartly.filter import filter_data
 from chartly import filter
 from chartly import graphs
 
 __all__ = [
-    "Chart",
-    "Filter",
-    "filter",
-    "graphs",
+    "Chart",  # class
+    "Filter",  # class
+    "filter",  # module
+    "filter_data",  # function
+    "graphs",  # module
 ]
